308b9fc0681ee9d82ca2071dd128a8715ea42676
JeongWogh <wogh06@gmail.com>
2024-10-07 16:15:53 +0900

    .

--- app/src/main/java/com/example/search_db_api/ApiClient.java
+++ app/src/main/java/com/example/search_db_api/ApiClient.java
@@ -5,8 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-     private static final String BASE_URL = "http://10.0.2.2:5000/";  // 가상 머신 쓸 떄 Flask 서버 주소
-//    private static final String BASE_URL = "http://10.100.1.152:5000/"; // 폰 쓸 때 서버 주소
+//     private static final String BASE_URL = "http://10.0.2.2:5000/";  // 가상 머신 쓸 떄 Flask 서버 주소
+    private static final String BASE_URL = "http://10.100.1.110:5000/"; // 폰 쓸 때 서버 주소
 //    private static final String BASE_URL = "http://192.168.1.9:5000/"; // 집에서 할 때
     private static Retrofit retrofit;
 
